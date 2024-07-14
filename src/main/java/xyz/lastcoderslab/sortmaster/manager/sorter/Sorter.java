53e6f2eb7b31fd089f5e891b87c3c5e2437537a8
powerpuff-luv <ardepresov@mail.com>
2024-07-15 02:27:32 +0300

    strand sort for list of integer

--- src/main/java/xyz/lastcoderslab/sortmaster/manager/sorter/Sorter.java
+++ src/main/java/xyz/lastcoderslab/sortmaster/manager/sorter/Sorter.java
@@ -3,5 +3,5 @@ package xyz.lastcoderslab.sortmaster.manager.sorter;
 import java.util.List;
 
 public interface Sorter {
-    void sort(List<Integer> list);
+    public List<Integer> sort(List<Integer> list);
 }
--- src/main/java/xyz/lastcoderslab/sortmaster/manager/sorter/StrandSorter.java
+++ src/main/java/xyz/lastcoderslab/sortmaster/manager/sorter/StrandSorter.java
@@ -1,35 +1,43 @@
 package xyz.lastcoderslab.sortmaster.manager.sorter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class StrandSorter implements Sorter {
 
     @Override
-    public void sort(List<Integer> array) {
-        int left = 0;
-        int right = array.size() - 1;
-
-        while (left < right) {
-            int i = left;
-            int j = right;
-
-            while (i < j) {
-                if (array.get(i) > array.get(j)) {
-                    swap(array, i, j);
-                    i++;
-                    j--;
-                } else {
-                    break;
-                }
-            }
-
-            right = j;
+    public List<Integer> sort(List<Integer> inputList) {
+        if (inputList.size() <= 1) {
+            return inputList;
         }
+        List<Integer> sublist = new ArrayList<>();
+        sublist.add(inputList.remove(0));
+        int i = 0;
+        while (i < inputList.size()) {
+            if(inputList.get(i) > sublist.get(sublist.size() - 1)) {
+                sublist.add(inputList.remove(i));
+            } else {
+                i++;
+            }
+        }
+
+        List<Integer> sortedSublist = new ArrayList<>(sublist);
+        List<Integer> remainingList = sort(inputList);
+
+        return mergeLists(sortedSublist, remainingList);
     }
 
-    private void swap(List<Integer> array, int i, int j) {
-        Integer temp = array.get(i);
-        array.set(i, array.get(j));
-        array.set(j, temp);
+    private static List<Integer> mergeLists(List<Integer> list1, List<Integer> list2) {
+        List<Integer> result = new ArrayList<>();
+        while (!list1.isEmpty() && !list2.isEmpty()) {
+            if (list1.get(0).compareTo(list2.get(0)) <= 0) {
+                result.add(list1.remove(0));
+            } else {
+                result.add(list2.remove(0));
+            }
+        }
+        result.addAll(list1);
+        result.addAll(list2);
+        return result;
     }
 }
