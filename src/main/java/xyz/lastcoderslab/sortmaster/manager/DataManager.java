25ae33bacfcbb64bcf0c9da558022e8216ff47fc
powerpuff-luv <ardepresov@mail.com>
2024-07-18 04:18:03 +0300

    write to file

--- src/main/java/xyz/lastcoderslab/sortmaster/manager/DataManager.java
+++ src/main/java/xyz/lastcoderslab/sortmaster/manager/DataManager.java
@@ -1,8 +1,11 @@
 package xyz.lastcoderslab.sortmaster.manager;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import net.datafaker.Faker;
 import xyz.lastcoderslab.sortmaster.entity.CompareMe;
 
+import java.io.*;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -89,5 +92,47 @@ public class DataManager {
 
     public String dataToPrintFull() {
         return dataToPrintFull(data);
+    }
+
+    public void writeDataToFile(String filename, Comparable[] data) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            if (data instanceof Integer[]) {
+                Integer[] intData = Arrays.stream(data).map(i -> (Integer) i).toArray(Integer[]::new);                writer.write(gson.toJson(intData));
+            } else {
+                writer.write(gson.toJson(data));
+            }
+        } catch (IOException e) {
+            System.out.println("Error writing to file: " + e.getMessage());
+        }
+    }
+
+    public Comparable[] readDataFromFile(String filename) {
+        Comparable[] data = new Comparable[0];
+        Gson gson = new Gson();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String json = "";
+            String line;
+            while ((line = reader.readLine()) != null) {
+                json += line;
+            }
+            if (data instanceof Integer[]) {
+                Integer[] intData = gson.fromJson(json, Integer[].class);
+                data = intData;
+            } else {
+                data = gson.fromJson(json, CompareMe[].class);
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading from file: " + e.getMessage());
+        }
+        return data;
+    }
+
+    public static void main(String[] args) {
+        Comparable[] data = {1, 2, 3};
+        DataManager dm = new DataManager();
+        dm.writeDataToFile("D:\\studying\\programming\\javaProjects\\AstonCourse\\SortMaster\\src\\main\\file.txt", data);
+        Comparable[] data2 = dm.readDataFromFile("D:\\studying\\programming\\javaProjects\\AstonCourse\\SortMaster\\src\\main\\file.txt");
+        System.out.println(Arrays.toString(data2));
     }
 }
